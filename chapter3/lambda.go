4ec072f6e7e850ec4d371425333cfbe86fd3ad58
Wataru Inoue <southavem@gmail.com>
2018-12-10 16:53:06 +0900

    Update lambda.go

--- chapter3/lambda.go
+++ chapter3/lambda.go
@@ -9,4 +9,7 @@ func main() {
     f = func(x, y int) int { return x + y }
     fmt.Println(f(2, 3))
     fmt.Printf("%T\n", f)
+
+    fmt.Printf("%#v\n", func(x, y int) int { return x + y } )
+    fmt.Printf("%#v\n", func(x, y int) int { return x + y }(2, 3) )
 }
