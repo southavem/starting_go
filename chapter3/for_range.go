5505fac4b741a3cbc905e04ba5035eafd6dc986e
Wataru Inoue <southavem@gmail.com>
2018-12-11 13:03:09 +0900

    Create for_range.go

---
+++ chapter3/for_range.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    fruits := [3]string{"Apple", "Banana", "Cherry"}
+
+    for i, s := range fruits {
+        fmt.Printf("fruits[%d]=%s\n", i, s)
+    }
+}
