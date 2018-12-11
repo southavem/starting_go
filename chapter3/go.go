09358347a326eaff693c7943d95fd7d4340cfbaa
Wataru Inoue <southavem@gmail.com>
2018-12-11 15:29:50 +0900

    Create go.go

---
+++ chapter3/go.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sub() {
+    for {
+        fmt.Println("sub loop")
+    }
+}
+
+func main() {
+    go sub()
+    for {
+        fmt.Println("main loop")
+    }
+}
