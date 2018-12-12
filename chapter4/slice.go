eb24b8665848c9e262b2812e7f259b3c50661005
Wataru Inoue <southavem@gmail.com>
2018-12-13 08:33:08 +0900

    Update slice.go

--- chapter4/slice.go
+++ chapter4/slice.go
@@ -6,4 +6,14 @@ func main() {
     s := make([]int, 10)
     fmt.Println(s)
 
+    a := make([]float64, 3)
+    fmt.Println(a)
+    a[0] = 3.14
+    fmt.Println(a)
+    a[1] = 6.28
+    fmt.Println(a)
+    fmt.Println(a[0])
+    // fmt.Println(a[3])
+
+    fmt.Println(len(a))
 }
