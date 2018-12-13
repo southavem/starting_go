3a0f2a17c94aaf3dbf9cb0169c5b6722af1ce6c9
Wataru Inoue <southavem@gmail.com>
2018-12-13 12:42:38 +0900

    Update type.go

--- chapter5/type.go
+++ chapter5/type.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type (
+    IntPair      [2]int
+    Strings      []string
+    AreaMap      map[string][2]float64
+ )
+
 func main() {
     type MyInt int
 
@@ -9,4 +15,11 @@ func main() {
     n2 := MyInt(7)
     fmt.Println(n1)
     fmt.Println(n2)
+
+    pair := IntPair{1, 2}
+    fmt.Println(pair)
+    strs := Strings{"Apple", "Banana", "Cherry"}
+    fmt.Println(strs)
+    amap := AreaMap{"Tokyo": {35.689488, 139.691706}}
+    fmt.Println(amap)
 }
