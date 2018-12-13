f38a04137fe337b8d87c829377aff257d0cc59d6
Wataru Inoue <southavem@gmail.com>
2018-12-13 15:47:34 +0900

    Update method.go

--- chapter5/method.go
+++ chapter5/method.go
@@ -8,7 +8,38 @@ func (p *Point) Render() {
     fmt.Printf("<%d,%d>\n", p.X, p.Y)
 }
 
+
+type IntPair [2]int
+
+func (ip IntPair) First() int {
+    return ip[0]
+}
+
+func (ip IntPair) Last() int {
+    return ip[1]
+}
+
+type Strings []string
+
+func (s Strings) Join(d string) string {
+    sum := ""
+    for _, v := range s {
+        if sum != "" {
+            sum += d
+        }
+        sum += v
+    }
+    return sum
+}
+
 func main() {
     p := &Point{X: 5, Y: 12}
     p.Render()
+
+    ip := IntPair{1, 2}
+    fmt.Println(ip.First())
+    fmt.Println(ip.Last())
+
+    fmt.Println(Strings{"A", "B", "C"}.Join(","))
 }
+
