0af1af0ce5309a72d704bd50976324abbbdde7b5
Wataru Inoue <southavem@gmail.com>
2018-12-14 14:43:24 +0900

    Create interface.go

---
+++ chapter5/interface.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    //"reflect"
+)
+
+type MyError struct {
+    Message string
+    ErrCode int
+}
+
+func (e *MyError) Error() string {
+    return e.Message
+}
+
+func RaiseError() error {
+    return &MyError{Message: "エラーが発生しました", ErrCode: 1234}
+}
+
+func main() {
+    err := RaiseError()
+    fmt.Println(err.Error())
+    // fmt.Println(err.ErrCode)
+}
